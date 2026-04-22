756e90cce72ea454d6507d4f6ad7791083b4c314
tommylee0923 <tommylee0923@gmail.com>
2026-04-22 09:28:05 -0400

    feat: added issue codes for type-level hints and instance properties

--- src/Intent.Contract/Validation/IssueCode.cs
+++ src/Intent.Contract/Validation/IssueCode.cs
@@ -8,28 +8,29 @@ namespace Intent.Contract.Validation
         // ---------------------------------------------------------------
         // Common
         // ---------------------------------------------------------------
-        public const string MissingSchemaVersion    = "COMMON_MISSING_SCHEMA_VERSION";
+        public const string MissingSchemaVersion = "COMMON_MISSING_SCHEMA_VERSION";
 
         // ---------------------------------------------------------------
         // identity
         // ---------------------------------------------------------------
-        public const string MissingStableId         = "WALL_MISSING_STABLE_ID";
-        public const string InvalidObjectType       = "WALL_INVALID_OBJECT_TYPE";
+        public const string MissingStableId = "WALL_MISSING_STABLE_ID";
+        public const string InvalidObjectType = "WALL_INVALID_OBJECT_TYPE";
 
         // ---------------------------------------------------------------
         // type-level hints
         // ---------------------------------------------------------------
-        public const string MissingTypeName         = "WALL_MISSING_TYPE_NAME";
-        public const string MissingNominalWidth     = "WALL_MISSING_NOMINAL_WIDTH";
-        public const string InvalidNominalWidth     = "WALL_INVALID_NOMINAL_WIDTH";
+        public const string MissingTypeName = "WALL_MISSING_TYPE_NAME";
+        public const string MissingNominalWidth = "WALL_MISSING_NOMINAL_WIDTH";
+        public const string InvalidNominalWidth = "WALL_INVALID_NOMINAL_WIDTH";
 
         // ---------------------------------------------------------------
         // instance properties
         // ---------------------------------------------------------------
-        public const string InvalidHeight           = "WALL_INVALID_HEIGHT";
-        public const string InvalidBaseOffset       = "WALL_INVALID_BASE_OFFSET";
-        public const string InvalidTopOffset        = "WALL_INVALID_TOP_OFFSET";
-        public const string MissingLocationLine     = "WALL_MISSING_LOCATION_LINE";
-        public const string MissingStructuralFlag   = "WALL_MISSING_STRUCTURAL_FLAG";
+        public const string InvalidHeight = "WALL_INVALID_HEIGHT";
+        public const string InvalidBaseOffset = "WALL_INVALID_BASE_OFFSET";
+        public const string InvalidTopOffset = "WALL_INVALID_TOP_OFFSET";
+        public const string InvalidOffsetCombination = "WALL_INVALID_OFFSET_COMBINATION";
+        public const string MissingLocationLine = "WALL_MISSING_LOCATION_LINE";
+        public const string MissingStructuralFlag = "WALL_MISSING_STRUCTURAL_FLAG";
     }
 }
